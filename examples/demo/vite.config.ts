8273bac927f208229d9fc6639486f59dc2922166
McCoy <achieve502938049@gmail.com>
2025-10-04 21:33:45 +0800

    test

--- examples/demo/vite.config.ts
+++ examples/demo/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+const isGitHubPages = process.env.DEPLOY_TARGET === 'github-pages'
+const base = isGitHubPages ? '/react-flip-page/' : '/'
 
 export default defineConfig({
+  base,
   build: {
     rollupOptions: {
       // external: ['react', 'react-dom'],
